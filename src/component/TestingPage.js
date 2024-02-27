565a1a6a50f0d514000566b2471b1381568bc38f
Vivedha Kanagaraj <87801035+kvivedha@users.noreply.github.com>
2024-02-27 14:29:24 +0530

    Update TestingPage.js

--- src/component/TestingPage.js
+++ src/component/TestingPage.js
@@ -2,35 +2,8 @@ import React, { useState } from "react";
 import Picker from "emoji-picker-react";
 
 function Testing() {
-//   const [chosenEmoji, setChosenEmoji] = useState(null);
-
-//   const onEmojiClick = (event, emojiObject) => {
-//     setChosenEmoji(emojiObject);
-//     console.log('emojiObject',emojiObject);
-//   }
-//   return (
-//     <>
-//     <h1 style={{textAlign: 'center'}}>Demo emoji picker</h1>
-//         <div style={{textAlign: 'center', marginLeft: '810px'}}>
-//             <Picker onEmojiClick={onEmojiClick}/>
-//             {chosenEmoji && <EmojiData chosenEmoji = {chosenEmoji} />}
-//         </div>
-//     </>
-//   );
-// }
-
-// const EmojiData = ({chosenEmoji}) => {
-//     <div style={{textAlign: 'center', marginRight: '810'}}>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <hr></hr>
-//         <strong>Names : </strong> {chosenEmoji.names}<br />
-//         <strong>Symbol : </strong> {chosenEmoji.emoji}
-//     </div>
-const [chosenEmoji, setChosenEmoji] = useState(null);
- 
-const onEmojiClick = (event, emojiObject) => {
+ const [chosenEmoji, setChosenEmoji] = useState(null);
+ const onEmojiClick = (event, emojiObject) => {
     setChosenEmoji(emojiObject);
     console.log('emojiObject',emojiObject.target);
 };
@@ -41,10 +14,7 @@ return (
         {chosenEmoji ? (
             <span>
                 Your Emoji:
-                <img
-                    style={{ width: "15px" }}
-                    src={chosenEmoji.target.src}
-                />
+                <img style={{ width: "15px" }} src={chosenEmoji.target.src} />
             </span>
         ) : (
             <span>No Emoji</span>
